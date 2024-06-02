0faf948a88001599e1616bad535a10a5a533d20e
AngeloidAlpha <angeloid_alpha@abv.bg>
2024-06-02 06:21:39 +0300

    02.June 1st

--- 02.Exercise/03.SetsAndDictionariesAdvanced/06.Wardrobe/Program.cs
+++ 02.Exercise/03.SetsAndDictionariesAdvanced/06.Wardrobe/Program.cs
@@ -1,2 +1,107 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Drawing;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split(" -> ");
+
+    string color = input[0];
+    string[] clothes = input[1].Split(",");
+
+    if (!wardrobe.ContainsKey(color))
+    {
+        wardrobe.Add(color, new Dictionary<string, int>());
+    }
+    foreach (var item in clothes)
+    {
+        if (!wardrobe[color].ContainsKey(item))
+        {
+            wardrobe[color].Add(item, 0);
+        }
+        // това са библютеките които сме създали
+        // Dictionary<string, Dictionary<string, int>> wardrobe
+        // wardrobe = Dictionary<string, Dictionary<string, int>>;
+        // wardrobe[color] = Dictionary<string, int>;
+        // wardrobe[color][item] = int;
+        wardrobe[color][item] += 1;
+
+    }
+}
+string[] lookupValues = Console.ReadLine().Split();
+
+foreach (var (color, clothes) in wardrobe)
+{
+    Console.WriteLine($"{color} clothes:");
+
+    foreach (var (item, count) in clothes)
+    {
+        string outputValue = $"* {item} - {count}";
+
+        if (color == lookupValues[0] && item == lookupValues[1])
+        {
+            outputValue += " (found!)";
+        }
+        Console.WriteLine(outputValue);
+    }
+}
+/*
+ * 
+4
+Blue -> dress,jeans,hat
+Gold -> dress,t-shirt,boxers
+White -> briefs,tanktop
+Blue -> gloves
+Blue dress
+
+result:
+Blue clothes:
+* dress - 1 (found!)
+* jeans - 1
+* hat - 1
+* gloves - 1
+Gold clothes:
+* dress - 1
+* t-shirt - 1
+* boxers - 1
+White clothes:
+* briefs - 1
+* tanktop - 1
+
+4
+Red -> hat
+Red -> dress,t-shirt,boxers
+White -> briefs,tanktop
+Blue -> gloves
+White tanktop
+
+result:
+Red clothes:
+* hat - 1
+* dress - 1
+* t-shirt - 1
+* boxers - 1
+White clothes:
+* briefs - 1
+* tanktop - 1 (found!)
+Blue clothes:
+* gloves - 1
+
+5
+Blue -> shoes
+Blue -> shoes,shoes,shoes
+Blue -> shoes,shoes
+Blue -> shoes
+Blue -> shoes,shoes
+Red tanktop
+
+result:
+Blue clothes:
+* shoes - 9
+
+*/
--- 02.Exercise/03.SetsAndDictionariesAdvanced/07.TheV-Logger/Program.cs
+++ 02.Exercise/03.SetsAndDictionariesAdvanced/07.TheV-Logger/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+Dictionary<string, HashSet<string>> vloggers = new();
+Dictionary<string, int[]> followersFollwing = new();
+
+while (input[0] != "Statistics")
+{
+    string vloggerName = input[0];
+    string command = input[1];
+    int follwer = 0;
+
+    if (command == "joined")
+    {
+        if (!vloggers.ContainsKey(command))
+        {
+            followersFollwing.Add(vloggerName, new int[1]);
+            vloggers.Add(vloggerName, new HashSet<string>());
+        }
+    }
+
+    else if (command == "followed")
+    {
+        string currentvlogger = input[2];
+        if (vloggers.ContainsKey(currentvlogger))
+        {
+            if (vloggerName != currentvlogger)
+            {
+                follwer++;
+                // followersFollwing[vloggerName];
+                vloggers[vloggerName].Add(currentvlogger);
+            }
+        }
+    }
+    input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+}
+int maxFollowers = 0;
+string maxFollowedPerson = string.Empty;
+foreach (var name in vloggers)
+{
+    int numberOfFollowers = name.Value.Count();
+    if (numberOfFollowers > maxFollowers)
+    {
+        maxFollowers = numberOfFollowers;
+        maxFollowedPerson = name.Key;
+    }
+}
+Console.WriteLine($"The V-Logger has a total of {vloggers.Keys.Count()} vloggers in its logs.");
+Console.WriteLine($"{maxFollowedPerson} : {maxFollowers} followers, {0} following");
+/*
+
+EmilConrad joined The V-Logger
+VenomTheDoctor joined The V-Logger
+Saffrona joined The V-Logger
+Saffrona followed EmilConrad
+Saffrona followed VenomTheDoctor
+EmilConrad followed VenomTheDoctor
+VenomTheDoctor followed VenomTheDoctor
+Saffrona followed EmilConrad
+Statistics
+
+result
+
+*/
