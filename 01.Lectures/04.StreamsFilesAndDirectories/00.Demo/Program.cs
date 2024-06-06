f953871a488ef5d35a67da728c16dbdf1382a9df
AngeloidAlpha <angeloid_alpha@abv.bg>
2024-06-06 06:47:36 +0300

    6.June 1st

---
+++ 01.Lectures/04.StreamsFilesAndDirectories/00.Demo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Drawing;
+//Disposable Inteface
+using (Person person = new Person())
+{
+    Console.WriteLine("In disposable object scope");
+}
+Console.WriteLine("Out of disposable object scope");
+
+// Interface
+IFigure square = new Square();
+IFigure rectangle = new Rectangle();
+List<IFigure> figures = new List<IFigure>();
+figures.Add(square);
+figures.Add(rectangle);
+foreach(IFigure figure in figures)
+{
+    figure.PrintSurface();
+}
+
+square.PrintSurface();
+rectangle.PrintSurface();
+
+public interface IFigure
+{ 
+    void PrintSurface();
+}
+public class Square : IFigure
+{
+    public Square()
+    {
+        Side = 4;
+    }
+
+    public int Side { get; set; }
+
+    public void PrintSurface()
+    {
+        Console.WriteLine(Side * Side);
+    }
+}
+
+public class Rectangle : IFigure
+{
+    public Rectangle()
+    {
+        Width = 5;
+        Hight = 10;
+    }
+
+    public int Width { get; set; }
+    public int Hight { get; set; }
+
+    public void PrintSurface()
+    {
+        Console.WriteLine(Width * Hight);
+    }
+}
+//Disposable Inteface
+
+
+public class Person : IDisposable
+{
+    public void Dispose()
+    {
+        Console.WriteLine("Disposing.");
+    }
+}
---
+++ 01.Lectures/04.StreamsFilesAndDirectories/01.OddLines/OddLines.cs
@@ -0,0 +1,29 @@
+﻿
+string inputFilePath = @"..\..\..\Files\input.txt";
+string outputFilePath = @"..\..\..\Files\output.txt";
+
+ExtractOddLines(inputFilePath, outputFilePath);
+
+static void ExtractOddLines(string inputFilePath, string outputFilePath)
+{
+    // TODO: write your code here…
+    using (StreamReader reader = new StreamReader(inputFilePath))
+    {
+        using (StreamWriter writer = new StreamWriter(outputFilePath))
+        {
+            int counter = 0;
+
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (counter % 2 != 0)
+                {
+                    writer.WriteLine(line);
+                }
+                counter++;
+            }
+        }
+    }
+}
+    
