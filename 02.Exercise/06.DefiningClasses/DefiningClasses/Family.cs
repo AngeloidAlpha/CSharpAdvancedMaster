7a7da7be8a05f731d9095358f270b5a03b8dfd97
AngeloidAlpha <angeloid_alpha@abv.bg>
2024-06-19 12:25:02 +0300

    19.June 1st

---
+++ 02.Exercise/06.DefiningClasses/DefiningClasses/Family.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    internal class Family
+    {
+        // конструктора е важен за класа (все едно е storage-a на класа)
+        public Family()
+        {
+            this.People = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+        public void AddMember(Person person)
+        {
+            this.People.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.People
+                .OrderByDescending(p => p.Age)
+                .FirstOrDefault();
+        }
+    }
+}
---
+++ 02.Exercise/06.DefiningClasses/DefiningClasses/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        
+        // генериране на конструктор
+        // ctor tab tab (празен) или селектирай пропъртитата и ctrl + .
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+        public Person (int age) : this()
+        {
+            this.Name = "No name";
+            this.Age = age;
+        }
+        public Person(string name, int age) : this(age)
+        {
+            this.Name = name;
+        }
+        // като вземаме стойност минаваме стъпка get
+        // като записваме стойност използваме часта set
+        public string Name { get { return name; } set { name = value; } } 
+        public int Age { get { return age; } set { age = value; } }
+    }
+}
